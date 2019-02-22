21c67f5c039bcdc520a6d7b3cb1f706d493b0472
aluno <ruancrepeszabel@hotmail.com>
2019-02-22 17:15:15 -0300

    criaçãoProjeto

--- app/src/main/java/br/edu/ifsc/numerosaleatorios/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/numerosaleatorios/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +19,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void alteraTexto(View view) {
+
         Button b = (Button)view;
-        b.setText("OLÁ");
+        TextView text=(TextView) findViewById(R.id.textView);
+        EditText edit =(EditText)findViewById(R.id.editText);
+        if(a==0){
+            b.setText("OLÁ");
+            text.setText(edit.getText());
+            a=1;
+        }else{
+            b.setText("ATÉ");
+            text.setText("volta mundo");
+            a=0;
+        }
+
+
+
 
     }
 }
